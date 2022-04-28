eeaf961f7ae7f170a1512b68274085ee087d930a
giotsere <giorgitsereteli@protonmail.com>
2022-04-28 20:36:55 +0300

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCibpzFQlbQvZrtW-PkeAuu6RkZ6f6I5Qg',
+  authDomain: 'devnews-cfd07.firebaseapp.com',
+  projectId: 'devnews-cfd07',
+  storageBucket: 'devnews-cfd07.appspot.com',
+  messagingSenderId: '10507188598',
+  appId: '1:10507188598:web:8abd6148b79a2fa443a257',
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);

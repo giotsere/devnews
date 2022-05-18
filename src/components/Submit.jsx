e704caa1d0618cdee60ae2e3edb8f7b1f1a01e20
giotsere <giorgitsereteli@protonmail.com>
2022-05-18 21:45:24 +0300

    Submit.jsx html done

--- src/components/Submit.jsx
+++ src/components/Submit.jsx
@@ -5,6 +5,21 @@ function Submit() {
   return (
     <div>
       <Navbar />
+      <div className="form m-auto mt-20">
+        <p className="bottom-margin form-title"> Title</p>
+        <input type="text" name="" id="" className="input-border  mb-4" />
+        <p className="bottom-margin form-title">URL</p>
+        <input type="text" name="" id="" className="input-border  mb-4" />
+        <p className="bottom-margin form-title">or Text</p>
+        <textarea
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+          className="input-border mb-4 resize-none"
+        ></textarea>
+        <button className="btn">Submit</button>
+      </div>
     </div>
   );
 }
